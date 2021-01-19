de3204deb14f7c25659706a9aafbec13a89fa457
rajitha1905 <rajitha.siripurapu@gmail.com>
2021-01-19 13:39:17 -0800

    change

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ echo "deployed"
 
 if [ "$TARGET_PLATFORM" == "bee_v1" ]; then echo "bee v2"; fi
 if [ "$TARGET_PLATFORM" == "bee_v3" ]; then  echo "bee v3"; fi
-if [ "$TARGET_PLATFORM" == "bee_er"]; then  echo "bee er"; fi
+if [ "$TARGET_PLATFORM" == "bee_er" ]; then  echo "bee er"; fi
 
