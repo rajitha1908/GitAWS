c40dc1e56779af55bfda5d7782f345efbdb38136
rajitha1908 <rajitha.siripurapu@gmail.com>
2021-01-19 11:34:07 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 export PATH= /var/jenkins/toolchain/gcc-arm-none-eabi-9-2020-q2-update/bin:$PATH
 echo 'deploy phase'
 echo "deployed"
-if [[$TARGET_PLATFORM == "bee_v1"]]; then  
+- if [[$TARGET_PLATFORM == "bee_v1"]]; then  
    ./usr/bin/make -f Makefile.swarmbee clean
    ./usr/bin/make -f Makefile.swarmbee; 
-fi
-if [[$TARGET_PLATFORM == "bee_v2"]]; then  echo "bee v2"; fi
-if [[${TARGET_PLATFORM} == "bee_v3"]]; then  echo "bee v3"; fi
-if [${TARGET_PLATFORM} == "bee_er"]; then  echo "bee er"; fi
+  fi
+- if [[$TARGET_PLATFORM == "bee_v2"]]; then  echo "bee v2"; fi
+- if [[${TARGET_PLATFORM} == "bee_v3"]]; then  echo "bee v3"; fi
+- if [${TARGET_PLATFORM} == "bee_er"]; then  echo "bee er"; fi
 
