ad96165123956198a558d624f25fc6721ab487c0
rajitha1905 <rajitha.siripurapu@gmail.com>
2021-01-19 13:32:23 -0800

    another change

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-export PATH= /var/jenkins/toolchain/gcc-arm-none-eabi-9-2020-q2-update/bin:$PATH
+#export PATH= /var/jenkins/toolchain/gcc-arm-none-eabi-9-2020-q2-update/bin:$PATH
 echo 'deploy phase'
 echo "deployed"
 
 if [ "$TARGET_PLATFORM" == "bee_v1" ]; then echo "bee v2"; fi
-if [ "${TARGET_PLATFORM}" == "bee_v3" ]; then  echo "bee v3"; fi
-if [${TARGET_PLATFORM} == "bee_er"]; then  echo "bee er"; fi
+if [ "$TARGET_PLATFORM" == "bee_v3" ]; then  echo "bee v3"; fi
+if [ "$TARGET_PLATFORM" == "bee_er"]; then  echo "bee er"; fi
 
