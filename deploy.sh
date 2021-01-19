7f4adb36e5a570f703db2e99da4f4f8f439dba47
rajitha1908 <rajitha.siripurapu@gmail.com>
2021-01-19 10:15:16 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 export PATH= /var/jenkins/toolchain/gcc-arm-none-eabi-9-2020-q2-update/bin:$PATH
 echo 'deploy phase'
 echo "deployed"
-file="../test.txt"
-echo "Adding first line" > $file
-echo "Adding first line replaced" > $file
-echo "Appending second line " >> $file
-echo "Appending third line" >> $file
-cat $file
+
