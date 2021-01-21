a2da1740bbc1ba6ef0cd576f1197a1f2e06d13e8
rajitha1905 <rajitha.siripurapu@gmail.com>
2021-01-20 17:10:46 -0800

    er tagged commit v1

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 echo 'deploy phase'
 echo "deployed"
 
-if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_v1" ]]; then echo "bee v1"; fi
-if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_v3" ]]; then  echo "bee v3"; fi
-if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_er" ]]; then  echo "bee er"; fi
+if [[ "$TRAVIS_COMMIT_MSG" =~ "v1" ]]; then echo "bee v1"; fi
+if [[ "$TRAVIS_COMMIT_MSG" =~ "v3" ]]; then  echo "bee v3"; fi
+if [[ "$TRAVIS_COMMIT_MSG" =~ "er" ]]; then  echo "bee er"; fi
