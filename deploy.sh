1cc990a89afa6d52004953eb14325fc672234f7c
rajitha1905 <rajitha.siripurapu@gmail.com>
2021-01-20 16:58:25 -0800

    v3 commit v1

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-echo "deploy phase"
+#!/bin/bash
+echo 'deploy phase'
 echo "deployed"
-tthe requirements have changed
-git add webpage.html
+
+if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_v1" ]]; then echo "bee v1"; fi
+if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_v3" ]]; then  echo "bee v3"; fi
+if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_er" ]]; then  echo "bee er"; fi
