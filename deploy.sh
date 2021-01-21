e643d6acb1e4ef7b502ad56aa370f661515e9364
rajitha1905 <rajitha.siripurapu@gmail.com>
2021-01-20 16:25:36 -0800

    bee_v3 commit bee_er

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo 'deploy phase'
 echo "deployed"
 
-if [ "$TARGET_PLATFORM" == "bee_v1" ]; then echo "bee v2"; fi
-if [ "$TARGET_PLATFORM" == "bee_v3" ]; then  echo "bee v3"; fi
-if [ "$TARGET_PLATFORM" == "bee_er" ]; then  echo "bee er"; fi
+if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_v1" ]]; then echo "bee v1"; fi
+if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_v3" ]]; then  echo "bee v3"; fi
+if [[ "$TRAVIS_COMMIT_MSG" =~ "bee_er" ]]; then  echo "bee er"; fi
 
