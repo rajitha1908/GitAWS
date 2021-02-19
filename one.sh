c7aac4e98a1ac9584e6637925a3a9d0ae3d2e082
rajitha1905 <rajitha.siripurapu@gmail.com>
2021-02-19 08:55:38 -0800

    kgjfhdgsf

--- one.sh
+++ one.sh
@@ -1,4 +1,7 @@
 echo "There is nothing here"
+QTDIR='c/Users/travis/build/arm-gcc/folder1/folder2/folder3/one.txt'
+PREFIX="$QTDIR"
+echo "$PREFIX"
 
 echo " [--skip-vcs] "
 echo " [--revision  "REVISION"] "
